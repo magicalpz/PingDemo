060b4428287f2b26d4722636c51ffdeffb3d1740
magicalpz <842134249@qq.com>
2024-03-14 16:04:37 +0800

    拖动列表交换数据

--- app/src/main/java/com/jyn/pingtest/ui/main/UrlAdapter.kt
+++ app/src/main/java/com/jyn/pingtest/ui/main/UrlAdapter.kt
@@ -1,7 +1,6 @@
 package com.jyn.pingtest.ui.main
 
 import android.annotation.SuppressLint
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
